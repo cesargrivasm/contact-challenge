2f04b7beca8b6522556ac43dc34df82d03ba026b
Nathaniel Calderon <>
2019-03-21 08:05:49 -0400

    Setting up workshop

---
+++ src/main/java/com/claro/cc/service/impl/PersonInfoServiceImpl.java
@@ -0,0 +1,11 @@
+package com.claro.cc.service.impl;
+
+import com.claro.cc.service.PersonInfoService;
+import com.claro.cc.service.dto.PersonFullDTO;
+
+public class PersonInfoServiceImpl implements PersonInfoService {
+    @Override
+    public PersonFullDTO save(PersonFullDTO personFullDTO) {
+        return null;
+    }
+}
